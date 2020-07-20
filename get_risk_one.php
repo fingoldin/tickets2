a750a73d4ec68c86c2067565fee4398eabf4cf42
fingoldin <vassilioskaxiras@gmail.com>
2020-07-19 22:50:27 -0400

    stash

--- get_risk_one.php
+++ get_risk_one.php
@@ -17,7 +17,10 @@ if(isset($_SESSION["checked_assoc"]) && isset($_POST["example"])) {
       $main_data = [];
       foreach($_SESSION["checked_assoc"][0][0] as $seq_idx => $sequence) {
         if(intval($sequence["idx"]) < 9) {
-          array_push($main_data, array_slice($_SESSION["testing_data"][0][0][intval($seq_idx)], 0, intval($sequence["idx"]) + 1));
+          array_push($main_data, [
+            "data" => array_slice($_SESSION["testing_data"][0][0][intval($seq_idx)],
+            "seq_idx" => $seq_idx
+          ], 0, intval($sequence["idx"]) + 1));
         }
       }
       shuffle($main_data);
@@ -25,10 +28,10 @@ if(isset($_SESSION["checked_assoc"]) && isset($_POST["example"])) {
       $data = [];
       foreach($main_data as $sequence) {
         if($added < 20) {
-          for($i = 0; $i < count($sequence); $i++) {
+          for($i = 0; $i < count($sequence["data"]); $i++) {
             array_push($data, [
-              "fixed" => $sequence[$i],
-              "seq_idx" => $seq_idx,
+              "fixed" => $sequence["data"][$i],
+              "seq_idx" => $sequence["seq_idx"],
               "choice_idx" => $i,
               "spinner" => $spinners[8 - $i]
             ]);
